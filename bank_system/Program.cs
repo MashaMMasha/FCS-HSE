7206db07a71bfd7c091a4eede2bc400b51b04777
Мария Мурашко <mamurashko@edu.hse.ru>
2025-03-19 15:16:34 +0300

    Added DI-container

---
+++ bank_system/Program.cs
@@ -0,0 +1,17 @@
+﻿using BankSystem;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static void Main()
+    {
+        var serviceProvider = new ServiceCollection().AddSingleton<BankFacade>().BuildServiceProvider();
+        var bank = serviceProvider.GetRequiredService<BankFacade>();
+        var commandQueue = new CommandQueue();
+
+        
+        commandQueue.AddCommand(new BankCommand(() => bank.CreateAccount("1000"), "Создание счета"));
+        
+        commandQueue.ProcessCommands();
+    }
+}
